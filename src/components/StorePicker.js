d2520a09508911a0ee73ce291a38db88ca32fc8e
Patryk Uran <patrykuran@MacBook-Air-Patryk.local>
2018-09-30 19:04:34 +0200

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+       return (
+           <React.Fragment>
+            <h1>Hi again</h1>
+            <h2>You can render sibling elements without parent dummy div with React.Fragment!</h2>
+           </React.Fragment>
+       )
+    }
+}
+
+export default StorePicker;

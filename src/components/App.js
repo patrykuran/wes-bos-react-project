7cb0e95b00e70f2254d56e178512f56ae238ace3
Patryk Uran <p.uran@plej02004.ad.plej.pl>
2018-10-04 18:30:26 +0200

    finish lesson 16 - update state, pass an argument into function from parent component into child component

--- src/components/App.js
+++ src/components/App.js
@@ -6,53 +6,59 @@ import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
 
 class App extends React.Component {
-  state = {
-    fishes: {},
-    order: {}
-  };
+    state = {
+        fishes: {},
+        order: {}
+    };
 
-  addFish = fish => {
-    // 1. make a copy of existing state - we never override it. Use spread operator to copy the whole object
-    const fishes = { ...this.state.fishes };
-    // 2. add new fish to fishes variable
-    fishes[`fish${Date.now()}`] = fish;
-    // 3. set new fishes object to state
-    this.setState({
-      fishes: fishes
-    });
-  };
+    addFish = fish => {
+        // 1. make a copy of existing state - we never override it. Use spread operator to copy the whole object
+        const fishes = {...this.state.fishes};
+        // 2. add new fish to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. set new fishes object to state
+        this.setState({
+            fishes: fishes
+        });
+    };
 
-  loadSampleFishes = () => {
-    this.setState({
-      fishes: sampleFishes
-    });
-  };
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        });
+    };
 
-  render() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="Seafood Yumm" />
-          <ul className="fishes">
-            {/*
+    addToOrder = (key) => {
+      const order = {...this.state.order};
+      order[key] = order[key] + 1 || 1;
+      this.setState({ order : order });
+    };
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Seafood Yumm"/>
+                    <ul className="fishes">
+                        {/*
             we cannot use map() on object, it's only for arrays. But we can
             map through keys of the object. key is a React built-in
             functionality key={key} gives a unique key.
           */}
 
-            {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
-            ))}
-          </ul>
-        </div>
-        <Order />
-        <Inventory
-          addFish={this.addFish}
-          loadSampleFishes={this.loadSampleFishes}
-        />
-      </div>
-    );
-  }
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+                        ))}
+                    </ul>
+                </div>
+                <Order/>
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,24 +1,31 @@
 import React from "react";
-import { formatPrice } from "../helpers";
+import {formatPrice} from "../helpers";
 
 class Fish extends React.Component {
-  render() {
-    {
-      /* ES6 destructuring */
+    render() {
+        {
+            /* ES6 destructuring */
+        }
+        const {image, name, price, desc, status} = this.props.details;
+        const isAvailable = status === 'available';
+
+        return (
+            <li className="menu-fish">
+                <img src={image} alt={name}/>
+                <h3 className="fish-name">
+                    {name}
+                    <span className="price">{formatPrice(price)}</span>
+                </h3>
+                <p>{desc}</p>
+                <button
+                    disabled={!isAvailable}
+                    onClick={() => this.props.addToOrder(this.props.index)}
+                >
+                    {isAvailable ? 'Add to Cart' : 'Sold Out!'}
+                </button>
+            </li>
+        );
     }
-    const { image, name, price, desc, status } = this.props.details;
-    return (
-      <li className="menu-fish">
-        <img src={image} alt={name} />
-        <h3 className="fish-name">
-          {name}
-          <span className="price">{formatPrice(price)}</span>
-        </h3>
-        <p>{desc}</p>
-        <button>Add to Cart</button>
-      </li>
-    );
-  }
 }
 
 export default Fish;
